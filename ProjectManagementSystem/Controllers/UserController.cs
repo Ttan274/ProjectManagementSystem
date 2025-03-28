ffc0e0d1b8e6945911f676cddfb5a10be71fa6cc
TanTalha <93757948+Ttan274@users.noreply.github.com>
2025-03-28 04:14:14 +0300

    User Controller
    User creation and Login  has created.

---
+++ ProjectManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagementSystem.Domain.Entities;
+using ProjectManagementSystem.Persistance.DbContext;
+using ProjectManagementSystem.ViewModel;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserViewModel userVM)
+        {
+            var user = await _userManager.FindByEmailAsync(userVM.Email);
+            if(user != null)
+            {
+                TempData["Error"] = "Email has been taken";
+                return View(userVM);
+            }
+
+            if(ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = "TestObject",
+                    Email = userVM.Email,
+                    Gender = Common.Enums.Gender.Male,
+                    TeamId = Guid.Parse("BB212DDB-0C15-4D63-8080-92BE98B309FD")
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, userVM.Password);
+
+                if (result.Succeeded)
+                {
+                    _context.Update(appUser);
+                    _context.SaveChanges();
+                    return RedirectToAction("Login", "User");
+                }
+                  
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid) return View(login);
+
+            var user = await _userManager.FindByEmailAsync(login.Email);
+
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                if(result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ProjectManagementSystem/ViewModel/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectManagementSystem.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
