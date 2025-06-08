660e067168e5c524a421ad68a68184c64d8c669f
Serhat60Karaca <91076040+Serhat60Karaca@users.noreply.github.com>
2025-06-08 14:31:28 +0300

    ui: added application infos menu item

--- ProjectManagementSystem/Models/Navbar/Navbar.cs
+++ ProjectManagementSystem/Models/Navbar/Navbar.cs
@@ -1,8 +1,16 @@
-﻿namespace ProjectManagementSystem.Models
+﻿using ProjectManagementSystem.Application.Abstractions.Project.Dto;
+
+namespace ProjectManagementSystem.Models
 {
     public class Navbar
     {
+        public Navbar()
+        {
+            Projects = [];
+            NavbarItems = [];
+        }
         public string? MainTitle { get; set; }
+        public List<ProjectDto> Projects { get; set; }
         public List<NavBarItem> NavbarItems { get; set; } = null!;
     }
 }
