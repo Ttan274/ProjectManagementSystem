900b793586771b11de1523dd9ccca42d3a699e32
Serhat60Karaca <91076040+Serhat60Karaca@users.noreply.github.com>
2025-06-20 14:08:27 +0300

    feat: added toggle for side menu

--- ProjectManagementSystem/Models/Navbar/NavBarItem.cs
+++ ProjectManagementSystem/Models/Navbar/NavBarItem.cs
@@ -4,6 +4,7 @@
     {
         public string? Name { get; set; }
         public string? Role { get; set; }
+        public string? IconClass { get; set; }
         public string? Title { get; set; }
         public string? Action { get; set; }
         public string? Controller { get; set; }
